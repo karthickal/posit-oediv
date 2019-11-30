ed2964a03c473813bf3341387062605cfe56725f
udhay24 <udhay@rivi.co>
2019-11-30 12:57:46 +0530

    added splash screen

---
+++ app/src/main/java/com/justplay/posit/splash/SplashFragment.kt
@@ -0,0 +1,30 @@
+package com.justplay.posit.splash
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.justplay.posit.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        runBlocking {
+            delay(1_000)
+
+        }
+    }
+}
