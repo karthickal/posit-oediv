1251a88c15da1fa2d185ba9f2827e3160f04825c
udhay24 <udhay@rivi.co>
2019-12-14 12:46:52 +0530

    added live video feature

--- app/src/main/java/com/justplay/posit/MainActivity.kt
+++ app/src/main/java/com/justplay/posit/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.justplay.posit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setupWithNavController(findNavController(R.id.nav_fragment))
     }
 }
